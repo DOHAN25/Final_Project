3e22df46dbd5cecc07f598db41aff1286d4b1522
dd-jiny <radiooio@naver.com>
2021-07-06 01:35:24 +0900

    에러 수정 머지를 위한 푸시

--- Final_Project04/src/main/java/com/phoenix/carrot/model/controller/CommentController.java
+++ Final_Project04/src/main/java/com/phoenix/carrot/model/controller/CommentController.java
@@ -3,7 +3,6 @@ package com.phoenix.carrot.model.controller;
 import org.springframework.stereotype.Controller;
 
 @Controller
-public class BoardController {
-	
+public class CommentController {
 
 }
